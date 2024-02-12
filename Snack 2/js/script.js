819e8529d0914595b77d46c933dc2ac0da64907e
michele-pupo <mikypupo@hotmail.it>
2024-02-12 12:55:11 +0100

    created function to merge the 2 arrays together

--- Snack 2/js/script.js
+++ Snack 2/js/script.js
@@ -28,3 +28,22 @@ let array2 = ['i',
               'aerei',
               'loro',
               '.'];
+
+
+// creiamo una funzione che fonda insieme i 2 array
+function foundationArray(array1, array2) {
+
+    // creiamo un array vuoto dove verranno inseriti i due array dondati insieme
+    let resultMergeArray = [];
+
+        // tramite un ciclo for iteriamo tra i due array
+        for (let i = 0; i < array1.length; i++) {
+                resultMergeArray.push(array1[i]);
+                resultMergeArray.push(array2[i]);
+            }
+                return resultMergeArray;
+            }
+
+// creiamo una costante per il nuovo array
+const resultMergePhrase = foundationArray(array1, array2);
+console.log(resultMergePhrase);
