a71a9153b4b19dbf6c70a459bf86479d07bdb6dd
michele-pupo <mikypupo@hotmail.it>
2024-02-12 17:00:21 +0100

    created user input with control

---
+++ Snack 5/js/script.js
@@ -0,0 +1,25 @@
+//  Snack 5 (bonus):
+//  - Scrivi una funzione che accetti una stringa come parametro e restituisca 
+//    la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
+//    Es: funzioneEsempio("nel mezzo del cammin di nostra vita")  
+//    :freccia_destra:  "Nel Mezzo Del Cammin Di Nostra Vita"
+
+
+// chiediamo all'utente di inserire una frase tramite prompt
+let input = prompt("Inserisci una frase:");
+
+// verifichiamo che l'utente abbia scritto qualcosa
+if (input === null || input === "") {
+
+    // se l'utente non ha scritto nulla
+    // console.log("Nessuna frase inserita.");
+    alert("Nessuna frase inserita, ricarica la pagina");
+
+} else {
+    
+    // altrimenti procediamo ad effettuare la conversione delle iniziali
+    
+}
+
+
+
