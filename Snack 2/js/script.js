bd20338cfce61798999533e593fbcca3acfc06ce
michele-pupo <mikypupo@hotmail.it>
2024-02-12 12:42:57 +0100

    created the 2 arrays for snack 2

---
+++ Snack 2/js/script.js
@@ -0,0 +1,30 @@
+// Snack 2:
+// - Scrivi una funzione che fonda due array presi come parametri 
+//   (date per scontato che abbiano lo stesso numero di elementi) 
+//   prendendo alternativamente gli elementi da uno e dall’altro
+// - es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+// creiamo i due array con lunghezza uguale
+let array1 = ['Misurare', 
+              'progressi', 
+              'programmazione',
+              'linee',
+              'codice',
+              'come',
+              'i',
+              'della',
+              'di',
+              'dal',
+              'peso'];
+
+let array2 = ['i', 
+              'della', 
+              'dalle',
+              'di',
+              'é',
+              'misurare',
+              'progressi',
+              'costruzione',
+              'aerei',
+              'loro',
+              '.'];
