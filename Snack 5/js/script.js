05aeb093bc41b92a147adac8bf0ba14906d753f4
michele-pupo <mikypupo@hotmail.it>
2024-02-12 17:33:27 +0100

    function added to make initials capitalised

--- Snack 5/js/script.js
+++ Snack 5/js/script.js
@@ -17,9 +17,36 @@ if (input === null || input === "") {
 
 } else {
     
-    // altrimenti procediamo ad effettuare la conversione delle iniziali
-    
+    // altrimenti procediamo ad effettuare la conversione delle iniziali tramite la funzione
+    let phraseCapitalInitials = capitalInitials(input);
+    console.log(phraseCapitalInitials);
+
+    // stampiamo in pagina la frase ottenuta
+    // creo una costante per stampare in pagina il risultato
+    const resultElement = document.querySelector("#capital-initials");
+
+    // stampo in pagina la frase ottenuta
+    resultElement.innerText = phraseCapitalInitials;
 }
 
 
+// creiamo una funzione per rendere le prime lettere delle parole maiuscole
+function capitalInitials(stringa) {
+    
+    // dividiamo la stringa in parole
+    let words = stringa.split(' ');
+    console.log(words);
+    
+    // iteriamo con un ciclo for su ogni parola dell'array ottenuto
+    for (let i = 0; i < words.length; i++) {
 
+        // prendiamo ogni parola che è contenuta nell'array e per ogni parola
+        // con .charAt(0), cioè al carattere posto per primo applichiamo .toUpperCase()
+        words[i] = words[i].charAt(0).toUpperCase();
+
+    }
+    
+    // il metodo .join(' ') ci restuirà una stringa presa dall'array con, in questo caso,
+    // uno spazio tra le parole
+    return words.join(' ');
+}
